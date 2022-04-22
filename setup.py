7f4f972ebe96f25e6e915fa7e90c144f5b98a64c
Khoerul Umam <khoerul.umam@gotoko.co.id>
2022-04-22 09:37:31 +0700

    [UPDATE] Clean emoji

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Sastrapy',
-    version='0.1.7',
+    version='0.1.9',
     description='A simple text processing for Bahasa Indonesia',
     long_description='A simple text processing for Bahasa Indonesia',
     url='https://github.com/sastrapy/sastrapy',
